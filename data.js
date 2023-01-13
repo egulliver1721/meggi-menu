090f18525a58fca07bb5cb3d8b5c29e9259708e9
egulliver1721 <118499423+egulliver1721@users.noreply.github.com>
2023-01-13 17:57:03 +0800

    Added cart functionality and local storage

---
+++ data.js
@@ -0,0 +1,32 @@
+import { v4 as uuidv4 } from "https://jspm.dev/uuid";
+
+export const tagData = [
+  {
+    item: "nappies",
+    category: "changing",
+    icon: "/diapers.png",
+    uuid: uuidv4(),
+    added: false,
+  },
+  {
+    item: "wipes",
+    category: "changing",
+    icon: "/wipes.png",
+    uuid: uuidv4(),
+    added: false,
+  },
+  {
+    item: "change mat",
+    category: "changing",
+    icon: "/mat.png",
+    uuid: uuidv4(),
+    added: false,
+  },
+  {
+    item: "nappy bags",
+    category: "changing",
+    icon: "/bag.png",
+    uuid: uuidv4(),
+    added: false,
+  },
+];
---
+++ index.js
@@ -0,0 +1,80 @@
+import { tagData } from "./data.js";
+import { v4 as uuidv4 } from "https://jspm.dev/uuid";
+let addedToCartArray = [];
+let removeBtn = document.getElementById("remove-btn");
+const cartFromLocalStorage = JSON.parse(
+  localStorage.getItem("addedToCartArray")
+);
+
+if (cartFromLocalStorage) {
+  addedToCartArray = cartFromLocalStorage;
+  cartHtml();
+}
+console.log(cartFromLocalStorage);
+
+document.addEventListener("click", function (e) {
+  if (e.target.dataset.add) {
+    handleAddClick(e.target.dataset.add);
+  }
+
+  if (e.target.dataset.remove) {
+    handleRemoveClick(e.target.dataset.remove);
+  }
+
+  // if (addedToCartArray.length > 0) {
+  //   document.getElementById("order-summary").classList.remove("no-items");
+  // }
+});
+
+function handleAddClick(itemId) {
+  const targetItemObj = tagData.filter(function (item) {
+    return item.uuid === itemId;
+  })[0];
+
+  if (!addedToCartArray.includes(targetItemObj.item)) {
+    addedToCartArray.push(targetItemObj.item);
+  }
+  localStorage.setItem("addedToCartArray", JSON.stringify(addedToCartArray));
+  cartHtml();
+}
+
+function handleRemoveClick(e) {
+  const targetItemObj = tagData.filter(function (item) {
+    return item.uuid === itemId;
+  })[0];
+}
+
+localStorage.removeItem("item");
+
+function cartHtml() {
+  let cartHtml = ``;
+  addedToCartArray.forEach(function (item) {
+    cartHtml += `<p>${item} <span class="remove" data-remove="${item.uuid}" id="remove-btn"> remove </span> </p>`;
+  });
+  renderCart(cartHtml);
+}
+
+function getAppHtml() {
+  let appHtml = ``;
+
+  tagData.forEach(function (item) {
+    appHtml += `<img src="${item.icon}" class="item-icon" alt="" />
+        <div class="item-name">${item.item}</div>
+        <img src="add.png" class="icon-add" data-add="${item.uuid}" alt="" />
+          `;
+  });
+
+  return appHtml;
+}
+
+function render() {
+  document.getElementById("item-section").innerHTML = getAppHtml();
+}
+
+function renderCart(cartHtml) {
+  document.getElementById("item-summary").innerHTML = cartHtml;
+}
+
+render();
+
+// localStorage.clear();
